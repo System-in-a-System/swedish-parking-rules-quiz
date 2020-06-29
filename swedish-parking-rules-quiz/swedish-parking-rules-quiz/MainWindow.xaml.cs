a9b83e04593a7b343398e367a4518c5ad6509720
System-in-a-System <textconstruction@protonmail.com>
2020-06-29 19:24:54 +0300

    Adding sendButton event handler

--- swedish-parking-rules-quiz/swedish-parking-rules-quiz/MainWindow.xaml.cs
+++ swedish-parking-rules-quiz/swedish-parking-rules-quiz/MainWindow.xaml.cs
@@ -48,15 +48,65 @@ namespace swedish_parking_rules_quiz
 
         // Initialize a random "picker"
         Random picker = new Random();
+        int pairID;
+
+
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
+            // Hide start button on click
+            startButton.Visibility = Visibility.Hidden;
+            
             // Enable the answer field and the send button
             answerField.IsEnabled = true;
             sendButton.IsEnabled = true;
 
             // Load quiz text to the text field using random picker to pick from the dictionary
-            quizText.Text = dic.ElementAt(picker.Next(0, dic.Count)).Key;
+            pairID = picker.Next(0, dic.Count);
+            quizText.Text = dic.ElementAt(pairID).Key;
+            
+        }
+
+
+        async private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Retrieve user answer from the answer field
+            string userAnswer = answerField.Text;
+
+            // Retireve the correct answer from the dictionary
+            string correctAnswer = dic.ElementAt(pairID).Value;
+
+
+            // Check user answer for correctness
+            if (userAnswer == correctAnswer)
+            {
+                answerField.Background = Brushes.ForestGreen;
+                statusLabel.Content = "Correct!";
+            }
+            else
+            {
+                answerField.Background = Brushes.Red;
+                statusLabel.Content = "Wrong! The correct answe is " + correctAnswer + ".";
+            }
+
+            // Pause to comprehend the feedback
+            await Task.Delay(3000);
+
+
+            
+            // Clean up
+            answerField.Text = "";
+            answerField.Background = Brushes.White;
+            statusLabel.Content = "";
+
+
+
+            // Move on to the next quiz card
+            // Load quiz text to the text field using random picker to pick from the dictionary
+            pairID = picker.Next(0, dic.Count);
+            quizText.Text = dic.ElementAt(pairID).Key;
+
+
         }
     }
 }
