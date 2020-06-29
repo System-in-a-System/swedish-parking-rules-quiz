d1a12c2f69d2e6afddc28b10cec7143a007a1f1e
System-in-a-System <textconstruction@protonmail.com>
2020-06-29 17:28:23 +0300

    Initializing a new WPF app

---
+++ swedish-parking-rules-quiz/swedish-parking-rules-quiz/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace swedish_parking_rules_quiz
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
